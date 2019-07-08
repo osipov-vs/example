b9f49ef3c9e27a1cb51ec5d73442e592427e062f
osipov-vs <osipov.vovka.ys@gmail.com>
2019-07-09 00:06:06 +0300

    add project files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.bloxPopup').lightboxController({
+        appendRegion: 'footer',
+        lightboxID:   'lightbox',
+        fadeDuration:   300,
+        contentFade:  300,
+        closeBtn:   true,
+    });
+});
